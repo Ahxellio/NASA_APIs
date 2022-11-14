f8815773c3bf96091146dfa8506df77025c7fe9a
Ahxello <vladis9484@gmail.com>
2022-11-14 22:31:11 +0300

    Добавьте файлы проекта.

---
+++ NASA_APIs.TestConsole/Program.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace NASA_APIs.TestConsole
+{
+    class Program
+    {
+
+        private static IHost _Hosting;
+
+        public static IHost Hosting => _Hosting ??= CreateHostBuilder(Environment.GetCommandLineArgs()).Build();
+
+        public static IServiceProvider Services => Hosting.Services;
+
+        public static IHostBuilder CreateHostBuilder(string[] args) => Host
+           .CreateDefaultBuilder(args)
+           .ConfigureServices(ConfigureServices);
+
+        private static void ConfigureServices(HostBuilderContext host, IServiceCollection services)
+        {
+            services.AddHttpClient<NASA_APIsClient>(client => client.BaseAddress = new Uri(host.Configuration["APOD"]));
+        }
+        static async Task Main(string[] args)
+        {
+            using var host = Hosting;
+            await host.StartAsync();
+            var apod = Services.GetRequiredService<NASA_APIsClient>();
+            var picture = await apod.GetAPOD(1);
+            Console.WriteLine("END!!!");
+            Console.ReadLine();
+            await host.StopAsync();
+        }
+    }
+}
---
+++ NASA_APIs/NASA_APIsClient.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+
+namespace NASA_APIs
+{
+    public class NASA_APIsClient
+    {
+        private readonly HttpClient _Client;
+        public NASA_APIsClient(HttpClient Client)=> _Client = Client;
+
+        public async Task<APOD[]> GetAPOD(int count)
+        {
+            return await _Client.GetFromJsonAsync<APOD[]>($"https://api.nasa.gov/planetary/apod?api_key=Q7ybo1n8FBtVagagquxxfZMX74TMiQcOTtxqIzSa&count={count}");
+        }
+
+    }
+    public class APOD
+    {
+        DateTime date { get; set; }
+        DateTime start_date { get; set; }
+        DateTime end_date { get; set; }
+        int count { get; set; }
+        bool thumbs { get; set; }
+
+    }
+}
