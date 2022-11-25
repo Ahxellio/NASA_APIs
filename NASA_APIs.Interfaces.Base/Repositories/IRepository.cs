3ab5d9636d84034cb8b42e5288bb2c145629e6d2
Ahxello <vladis9484@gmail.com>
2022-11-25 21:08:42 +0300

    Repository logic for working with the DB using EFCore tools

--- NASA_APIs.Interfaces.Base/Repositories/IRepository.cs
+++ NASA_APIs.Interfaces.Base/Repositories/IRepository.cs
@@ -11,41 +11,41 @@ namespace NASA_APIs.Interfaces.Base.Repositories
     public interface IRepository<T> where T : IEntity
     {
         //Проверяет есть ли сущность с указанным Id
-        Task<bool> ExistId(int id, CancellationToken cancellationToken = default);
+        Task<bool> ExistId(int Id, CancellationToken Cancel = default);
 
         //Проверяет есть ли целиком сущность(Проверяет равенство двух сущностей по равенсту их ID)
-        Task<bool> Exist(T item, CancellationToken cancellationToken = default);
+        Task<bool> Exist(T item, CancellationToken Cancel = default);
 
         //Сколько всего сущностей
-        Task<int> GetCount(, CancellationToken cancellationToken = default);
+        Task<int> GetCount(CancellationToken Cancel = default);
 
         //Извлечь всё
-        Task<IEnumerable<T>> GetAll(, CancellationToken cancellationToken = default);
+        Task<IEnumerable<T>> GetAll(CancellationToken Cancel = default);
 
         //Извлекает всё, но есть возможность пропустить некоторое число в начале
         //и выбрать указанное кол-во значений
-        Task<IEnumerable<T>> Get(int Skip, int Count, CancellationToken cancellationToken = default);
+        Task<IEnumerable<T>> Get(int Skip, int Count, CancellationToken Cancel = default);
 
         //Постраничное разбиение (Указываем страницу и сколько на ней должно быть эл-ов)
-        Task<IPage<T>> GetPage(int PageIndex, int PageSize, CancellationToken cancellationToken = default);
+        Task<IPage<T>> GetPage(int PageIndex, int PageSize, CancellationToken Cancel = default);
 
         //Найти сущность по Id(Возвращает null, если объект не найден)
-        Task<T> GetById(int Id, CancellationToken cancellationToken = default);
+        Task<T> GetById(int Id, CancellationToken Cancel = default);
 
         //Добавить объект в репозиторий
         Task<T> Add(T item, CancellationToken cancellationToken = default);
 
         //Внести изменния в сущность, хранимую в репозитории
         //(Будет возвращена сущность, которая хранилась в репозитори) / null
-        Task<T> Update(T item, CancellationToken cancellationToken = default);
+        Task<T> Update(T item, CancellationToken Cancel = default);
 
         //Удаление сущности из репозитория
         //(Будет возвращена сущность, которая хранилась в репозитори) / null
-        Task<T> Delete(T item, CancellationToken cancellationToken = default);
+        Task<T> Delete(T item, CancellationToken Cancel = default);
 
         //Удаление сущности из репозитория по Id
         //(Будет возвращена сущность, которая хранилась в репозитори) / null
-        Task<T> DeleteById(T item, CancellationToken cancellationToken = default);
+        Task<T> DeleteById(int Id, CancellationToken Cancel = default);
 
 
     }
