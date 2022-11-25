40f24a8da65f7d6a54888cb620f985a16abf0555
Ahxello <vladis9484@gmail.com>
2022-11-25 18:43:34 +0300

    Revision of the Repository Interface

--- NASA_APIs.Interfaces.Base/Repositories/IRepository.cs
+++ NASA_APIs.Interfaces.Base/Repositories/IRepository.cs
@@ -3,38 +3,62 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace NASA_APIs.Interfaces.Base.Repositories
 {
     public interface IRepository<T> where T : IEntity
     {
-        Task<bool> ExistId(int id);
-        Task<bool> Exist(T item);
-        Task<int> GetCount();
-        Task<IEnumerable<T>> GetAll();
+        //Проверяет есть ли сущность с указанным Id
+        Task<bool> ExistId(int id, CancellationToken cancellationToken = default);
 
-        Task<IEnumerable<T>> Get(int Skip, int Count);
-        Task<IPage<T>> GetPage(int PageIndex, int PageSize);
+        //Проверяет есть ли целиком сущность(Проверяет равенство двух сущностей по равенсту их ID)
+        Task<bool> Exist(T item, CancellationToken cancellationToken = default);
 
-        Task<T> GetById(int Id);
+        //Сколько всего сущностей
+        Task<int> GetCount(, CancellationToken cancellationToken = default);
 
-        Task<T> Add(T item);
+        //Извлечь всё
+        Task<IEnumerable<T>> GetAll(, CancellationToken cancellationToken = default);
 
-        Task<T> Update(T item);
+        //Извлекает всё, но есть возможность пропустить некоторое число в начале
+        //и выбрать указанное кол-во значений
+        Task<IEnumerable<T>> Get(int Skip, int Count, CancellationToken cancellationToken = default);
 
-        Task<T> Delete(T item);
+        //Постраничное разбиение (Указываем страницу и сколько на ней должно быть эл-ов)
+        Task<IPage<T>> GetPage(int PageIndex, int PageSize, CancellationToken cancellationToken = default);
 
-        Task<T> DeleteById(T item);
+        //Найти сущность по Id(Возвращает null, если объект не найден)
+        Task<T> GetById(int Id, CancellationToken cancellationToken = default);
+
+        //Добавить объект в репозиторий
+        Task<T> Add(T item, CancellationToken cancellationToken = default);
+
+        //Внести изменния в сущность, хранимую в репозитории
+        //(Будет возвращена сущность, которая хранилась в репозитори) / null
+        Task<T> Update(T item, CancellationToken cancellationToken = default);
+
+        //Удаление сущности из репозитория
+        //(Будет возвращена сущность, которая хранилась в репозитори) / null
+        Task<T> Delete(T item, CancellationToken cancellationToken = default);
+
+        //Удаление сущности из репозитория по Id
+        //(Будет возвращена сущность, которая хранилась в репозитори) / null
+        Task<T> DeleteById(T item, CancellationToken cancellationToken = default);
 
 
     }
     public interface IPage<T>
     {
         IEnumerable<T> Items { get; }
+        //Всего эл-ов
         int TotalCount { get; }
+        //Номер страницы
         int PageIndex { get; }
+        //Размер страницы
         int PageSize { get; }
+        //Полное кол-во страниц
         int TotalPagesCount => (int)Math.Ceiling((double)TotalCount / PageSize);
     }
 }
