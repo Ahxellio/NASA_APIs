c69c34e69b05e78fbc53bd0053a877144f14277b
Ahxello <vladis9484@gmail.com>
2022-11-28 20:41:47 +0300

    Adeded  methods to DbSourcesController Removed warnings when calling the Get method

---
+++ NASA_APIs.API/Controllers/DataSourcesController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NASA_APIs.DAL.Entities;
+using NASA_APIs.Interfaces.Base.Repositories;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace NASA_APIs.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DataSourcesController : ControllerBase
+    {
+        private readonly IRepository<DataSource> _Repository;
+
+        public DataSourcesController(IRepository<DataSource> Repository) => _Repository = Repository;
+
+        [HttpGet("count")]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(int))]
+        public async Task<IActionResult> GetItemsCount() => Ok(await _Repository.GetCount());
+        [HttpGet("exist/id/{id:int}")]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(bool))]
+        [ProducesResponseType(StatusCodes.Status404NotFound, Type = typeof(bool))]
+        public async Task<IActionResult> ExistId(int id) => await _Repository.ExistId(id) ? Ok(true) : NotFound(false);
+
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(bool))]
+        public async Task<IActionResult> GetAll() => Ok(await _Repository.GetAll());
+
+        [HttpGet("items[[{Skip:int}:{Count:int}]]")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public async Task<ActionResult<IEnumerable<DataSource>>> Get(int Skip, int Count) => Ok(await _Repository.Get(Skip, Count));
+    }
+}
