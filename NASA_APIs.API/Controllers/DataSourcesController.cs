a6cee680fc6f024b7791fcb91f8fe5ad508d72ff
Ahxello <vladis9484@gmail.com>
2022-11-29 20:44:40 +0300

    Finished DataSourcesController

--- NASA_APIs.API/Controllers/DataSourcesController.cs
+++ NASA_APIs.API/Controllers/DataSourcesController.cs
@@ -19,11 +19,20 @@ namespace NASA_APIs.API.Controllers
 
         [HttpGet("count")]
         [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(int))]
+
         public async Task<IActionResult> GetItemsCount() => Ok(await _Repository.GetCount());
         [HttpGet("exist/id/{id:int}")]
         [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(bool))]
         [ProducesResponseType(StatusCodes.Status404NotFound, Type = typeof(bool))]
-        public async Task<IActionResult> ExistId(int id) => await _Repository.ExistId(id) ? Ok(true) : NotFound(false);
+        public async Task<IActionResult> ExistId(int id) => 
+            await _Repository.ExistId(id) ? Ok(true) : NotFound(false);
+
+        [HttpGet("exist")]
+        [HttpPost("exist")]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(bool))]
+        [ProducesResponseType(StatusCodes.Status404NotFound, Type = typeof(bool))]
+        public async Task<IActionResult> Exist(DataSource item) => 
+            await _Repository.Exist(item) ? Ok(true) : NotFound(false);
 
         [HttpGet]
         [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(bool))]
@@ -31,7 +40,8 @@ namespace NASA_APIs.API.Controllers
 
         [HttpGet("items[[{Skip:int}:{Count:int}]]")]
         [ProducesResponseType(StatusCodes.Status200OK)]
-        public async Task<ActionResult<IEnumerable<DataSource>>> Get(int Skip, int Count) => Ok(await _Repository.Get(Skip, Count));
+        public async Task<ActionResult<IEnumerable<DataSource>>> Get(int Skip, int Count) => 
+            Ok(await _Repository.Get(Skip, Count));
 
 
         [HttpGet("page/{PageIndex:int}/{PageSize:int}")]
@@ -43,6 +53,49 @@ namespace NASA_APIs.API.Controllers
             var result = await _Repository.GetPage(PageIndex, PageSize);
             return result.Items.Any() ? Ok(result) : NotFound(result);  
         }
-    
+
+        [HttpGet("{id:int}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> GetById(int id) => 
+            await _Repository.GetById(id) is { } item ? Ok(item) : NotFound();
+
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        public async Task<IActionResult> Add(DataSource item)
+        {
+            var result = await _Repository.Add(item);
+            return CreatedAtAction(nameof(GetById), new { id = result.Id }, result);
+        }
+
+        [HttpPut]
+        [ProducesResponseType(StatusCodes.Status202Accepted)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> Update(DataSource item)
+        {
+            if (await _Repository.Update(item) is not { } result)
+                return NotFound(item);
+            return AcceptedAtAction(nameof(GetById), new { id = result.Id }, result);
+        }
+
+        [HttpDelete()]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> Delete(DataSource item)
+        {
+            if (await _Repository.Delete(item) is not { } result)
+                return NotFound(item);
+            return Ok(result);
+        }
+
+        [HttpDelete("{id:int}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> DeleteById(int Id)
+        {
+            if (await _Repository.DeleteById(Id) is not { } result)
+                return NotFound(Id);
+            return Ok(result);
+        }
     }
 }
